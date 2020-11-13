97d6a9f9088a7a1a5e42523d69d7e3c79f3bd488
Guillermo Peitzner <guillermopeitzner@gmail.com>
2020-11-13 14:55:58 -0600

    [add] python and rabbitmq integration

--- python_server/app.py
+++ python_server/app.py
@@ -1,11 +1,31 @@
-import requests
-from flask import Flask, jsonify, request
-from flask_cors import CORS
-
-app = Flask(__name__)
-CORS(app)
+import pika
+import sys
+import os
 
 
-@app.route("/", methods=["GET"])
-def route():
-    return jsonify({"message": "Hello world"})
+def main():
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(host='localhost'))
+    channel = connection.channel()
+
+    channel.queue_declare(queue='hello')
+
+    def callback(ch, method, properties, body):
+        print(" [x] Received %r" % body)
+
+    channel.basic_consume(
+        queue='hello', on_message_callback=callback, auto_ack=True)
+
+    print(' [*] Waiting for messages. To exit press CTRL+C')
+    channel.start_consuming()
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('Interrupted')
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
